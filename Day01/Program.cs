046a5842aa258b8a6412feee6b032081d10685c5
Joel Kaneshiro <joel8800@gmail.com>
2023-12-01 01:44:30 -0800

    Completed Day01

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,3 +1,99 @@
 ﻿using AoCUtils;
 
-Console.WriteLine("Day01:");
+Console.WriteLine("Day01: Trebuchet?!");
+
+string[] input = FileUtil.ReadFileByLine("input.txt");
+
+int part1 = 0;
+int part2 = 0;
+
+foreach (string line in input)
+{
+    int first = 0;
+    int last = 0;
+
+    for (int i = 0; i < line.Length; i++)
+    {
+        first = IsDigit(line.Substring(i));
+        if (first != -1)
+            break;
+    }
+
+    for (int i = line.Length - 1; i >= 0; i--)
+    {
+        last = IsDigit(line.Substring(i));
+        if (last != -1)
+            break;
+    }
+
+    part1 += (first * 10) + last;
+}
+
+Console.WriteLine(part1);
+
+
+foreach (string line in input)
+{
+    int digits = 0;
+    int first = 0;
+    int last = 0;
+    for (int i = 0; i < line.Length; i++)
+    {
+        first = IsReal(line.Substring(i));
+        if (first != -1)
+            break;
+    }
+
+    for (int i = line.Length - 1; i >= 0 ; i--)
+    {
+        last = IsReal(line.Substring(i));
+        if (last != -1)
+            break;
+    }
+
+    digits = (first * 10) + last;
+    part2 += digits;
+}
+
+Console.WriteLine(part2);
+
+//============================================================================
+
+int IsDigit(string line)
+{
+    int digit = -1;
+
+    if (char.IsDigit(line[0]))
+        digit = int.Parse(line[0].ToString());
+
+    return digit;
+}
+
+int IsReal(string line)
+{
+    if (char.IsDigit(line[0]))
+        return int.Parse(line[0].ToString());
+
+    if (line.StartsWith("one"))
+        return 1;
+    if (line.StartsWith("two"))
+        return 2;
+    if (line.StartsWith("three"))
+        return 3;
+    if (line.StartsWith("four"))
+        return 4;
+    if (line.StartsWith("five"))
+        return 5;
+    if (line.StartsWith("six"))
+        return 6;
+    if (line.StartsWith("seven"))
+        return 7;
+    if (line.StartsWith("eight"))
+        return 8;
+    if (line.StartsWith("nine"))
+        return 9;
+    if (line.StartsWith("zero"))
+        return 0;
+
+    return -1;
+}
--- README.md
+++ README.md
@@ -4,9 +4,10 @@
 
 #### Progress:
 <img style="display: block; margin-left: auto; margin-right: auto; width: 100%;"
-	src="https://progress-bar.dev/0/?scale=50&title=StarsCollected&width=700&suffix=/50"
+	src="https://progress-bar.dev/2/?scale=50&title=StarsCollected&width=700&suffix=/50"
 	alt="Progress Bar">
 </img>
 | Day | Status | Source | Solution Notes |
  | - | - | - | - |
+ | [Day 01:  Trebuchet?!](https://adventofcode.com/2023/day/1) | :star::star: | [Solution](./Day01/Program.cs) |  |
 
