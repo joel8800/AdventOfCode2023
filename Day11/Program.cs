95dd0514a1eb4029767090d52dd942d6d6d7eb42
Joel Kaneshiro <joel8800@gmail.com>
2023-12-11 16:20:57 -0800

    Completed Day11

---
+++ Day11/Program.cs
@@ -0,0 +1,90 @@
+﻿using AoCUtils;
+
+Console.WriteLine("Day11: Cosmic Expansion");
+
+List<List<char>> input = FileUtil.ReadFileToCharGrid("input.txt");
+
+List<(int r, int c)> galaxies = [];
+List<(int r, int c)> galsPt1 = [];
+List<(int r, int c)> galsPt2 = [];
+List<int> emptyRows = [];
+List<int> emptyCols = [];
+List<int> distancesPt1 = [];
+List<int> distancesPt2 = [];
+
+// find empty rows
+for (int i = 0; i < input.Count; i++)
+    if (input[i].Contains('#') == false)
+        emptyRows.Add(i);
+
+// find empty columns
+for (int col = 0; col < input[0].Count; col++)
+{
+    List<char> colData = [];
+    for (int row = 0; row < input.Count; row++)
+        colData.Add(input[row][col]);
+    if (colData.Contains('#') == false)
+        emptyCols.Add(col);
+}
+
+// find galaxies
+for (int row = 0; row < input.Count; row++)
+    for (int col = 0; col < input[0].Count; col++)
+        if (input[row][col] == '#')
+            galaxies.Add((row, col));
+
+int expFactor = 1; // 2 - 1
+
+// adjust galaxy locations for expansion
+for (int i = 0; i < galaxies.Count; i++)
+    galsPt1.Add(CalcNewPosition(galaxies[i], emptyRows, emptyCols, expFactor));
+
+// find distances
+for (int i = 0; i < galsPt1.Count - 1; i++)
+    for (int j = i + 1; j < galsPt1.Count; j++)
+        distancesPt1.Add(Manhattan(galsPt1[i], galsPt1[j]));
+
+Console.WriteLine($"Part1: {distancesPt1.Sum()}");   //9418609
+
+//----------------------------------------------------------------------------
+
+expFactor = 1000000 - 1;
+
+// adjust galaxy locations for expansion
+for (int i = 0; i < galaxies.Count; i++)
+    galsPt2.Add(CalcNewPosition(galaxies[i], emptyRows, emptyCols, expFactor));
+
+// find distances
+for (int i = 0; i < galsPt2.Count - 1; i++)
+    for (int j = i + 1; j < galsPt2.Count; j++)
+        distancesPt2.Add(Manhattan(galsPt2[i], galsPt2[j]));
+
+long sum = distancesPt2.Select(d => Convert.ToInt64(d)).Sum();
+
+Console.WriteLine($"Part2: {distancesPt2.Select(d => Convert.ToInt64(d)).Sum()}");  // 593821230983
+
+//============================================================================
+
+int Manhattan((int r, int c) point0, (int r, int c) point1)
+{
+    return Math.Abs(point0.r - point1.r) + Math.Abs(point0.c - point1.c);
+}
+
+int CalcPositionInRowOrCol(List<int> list, int val, int expFactor)
+{
+    for (int i = 0; i < list.Count; i++)
+    {
+        if (val < list[i])
+            return (i * expFactor) + val;
+    }
+
+    return list.Count * expFactor + val;
+}
+
+(int r, int c) CalcNewPosition((int r, int c) gal, List<int> emptyRows, List<int> emptyCols, int expFactor)
+{
+    int newRow = CalcPositionInRowOrCol(emptyRows, gal.r, expFactor);
+    int newCol = CalcPositionInRowOrCol(emptyCols, gal.c, expFactor);
+
+    return (newRow, newCol);
+}
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 #### Progress:
 <img style="display: block; margin-left: auto; margin-right: auto; width: 100%;"
-	src="https://progress-bar.dev/17/?scale=50&title=StarsCollected&width=700&suffix=/50"
+	src="https://progress-bar.dev/21/?scale=50&title=StarsCollected&width=700&suffix=/50"
 	alt="Progress Bar">
 </img>
 | Day | Status | Source | Solution Notes |
@@ -13,9 +13,11 @@
  | [Day 02:  Cube Conundrum](https://adventofcode.com/2023/day/2) | :star::star: | [Solution](./Day02/Program.cs) |  |
  | [Day 03:  Gear Ratios](https://adventofcode.com/2023/day/3) | :star::star: | [Solution](./Day03/Program.cs) |  |
  | [Day 04:  Scratchcards](https://adventofcode.com/2023/day/4) | :star::star: | [Solution](./Day04/Program.cs) |  |
- | [Day 05:  If You Give A Seed A Fertilizer](https://adventofcode.com/2023/day/5) | :star: | [Solution](./Day05/Program.cs) |  |
+ | [Day 05:  If You Give A Seed A Fertilizer](https://adventofcode.com/2023/day/5) | :star::star: | [Solution](./Day05/Program.cs) |  |
  | [Day 06:  Wait For It](https://adventofcode.com/2023/day/6) | :star::star: | [Solution](./Day06/Program.cs) |  |
  | [Day 07:  Camel Cards](https://adventofcode.com/2023/day/7) | :star::star: | [Solution](./Day07/Program.cs) |  |
  | [Day 08:  Haunted Wasteland](https://adventofcode.com/2023/day/8) | :star::star: | [Solution](./Day08/Program.cs) |  |
  | [Day 09:  Mirage Maintenance](https://adventofcode.com/2023/day/9) | :star::star: | [Solution](./Day09/Program.cs) |  |
+ | [Day 10:  Pipe Maze](https://adventofcode.com/2023/day/10) | :star: | [Solution](./Day10/Program.cs) |  |
+ | [Day 11:  Cosmic Expansion](https://adventofcode.com/2023/day/11) | :star::star: | [Solution](./Day11/Program.cs) |  |
 
