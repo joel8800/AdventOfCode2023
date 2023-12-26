ec2788c9919f37aba0a4cfb9cb857246f55de049
Joel Kaneshiro <joel8800@gmail.com>
2023-12-25 16:21:50 -0800

    Completed Day12 part 1

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -1,38 +1,107 @@
 ﻿using AoCUtils;
-using System.Text.RegularExpressions;
+using System.Text;
 
 Console.WriteLine("Day12: Hot Springs");
 
-string[] input = FileUtil.ReadFileByLine("inputSample.txt");
+string[] input = FileUtil.ReadFileByLine("input.txt");
 int combos = 0;
+
+foreach (string line in input)
+    combos += CombosInRow(line);
+
+Console.WriteLine($"Part1: {combos}");
+
 foreach (string line in input)
 {
-    string[] cr = line.Split(' ');
-    string grpStr = Regex.Replace(cr[0], @"\.+", ".");
-    List<string> groups = grpStr.Split('.', StringSplitOptions.RemoveEmptyEntries).ToList();
-    List<int> counts = cr[1].Split(',').Select(x => int.Parse(x)).ToList();
-
-    if (groups.Count == counts.Count)
-    {
-        for (int i = 0; i < groups.Count; i++)
-            combos += CombosInGroup(groups[i], counts[i]);
-    }
-    else
-    {
-        Console.WriteLine("not implemented");
-    }
 
 }
 
 
-Console.WriteLine($"Part1: {0}");
 Console.WriteLine($"Part2: {0}");
 
 //============================================================================
 
-int CombosInGroup(string group, int count)
+int CombosInRow(string entry)
 {
     int combos = 0;
 
+    string row = entry.Split(' ')[0];
+    string counts = entry.Split(' ')[1];
+    int unknown = entry.Where(c => c == '?').Count();
+
+    Console.WriteLine($"unknowns: {unknown}");
+    //Console.WriteLine($"{row} -- {counts}");
+
+    for (int i = 0; i < Math.Pow(2, unknown); i++)
+    {
+        List<char> binary = [.. Convert.ToString(i, 2).PadLeft(unknown, '0')];
+        StringBuilder sb = new();
+
+        //Console.Write($"-- binary:{Convert.ToString(i, 2).PadLeft(unknown, '0')} ");
+
+        foreach (char c in row)
+        {
+            if (c != '?')
+            {
+                sb.Append(c == '.' ? '.' : '#');
+            }
+            else
+            {
+                sb.Append(binary[0] == '0' ? '.' : '#');
+                binary.RemoveAt(0);
+            }
+        }
+
+        string result = CountRuns(sb.ToString());
+        //Console.WriteLine($"- {sb} -- {result}");
+
+        if (result == counts)
+            combos++;
+    }
+
+    //Console.WriteLine($"combos found: {combos}");
     return combos;
+}
+
+string CountRuns(string row)
+{
+    List<int> runs = [];
+
+    bool isInRun = false;
+    int count = 0;
+    foreach (char c in row)
+    {
+        if (isInRun)
+        {
+            if (c == '.')
+            {
+                isInRun = false;
+                runs.Add(count);
+                count = 0;
+            }
+            else
+                count++;
+        }
+        else
+        {
+            if (c == '#')
+            {
+                isInRun = true;
+                count = 1;
+            }
+        }
+    }
+
+    if (isInRun)
+        runs.Add(count);
+
+    StringBuilder sb = new();
+    for (int i = 0; i < runs.Count; i++)
+    {
+        sb.Append(runs[i]);
+        if (i < runs.Count - 1)
+            sb.Append(',');
+    }
+
+    return sb.ToString();
 }
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 #### Progress:
 <img style="display: block; margin-left: auto; margin-right: auto; width: 100%;"
-	src="https://progress-bar.dev/33/?scale=50&title=StarsCollected&width=700&suffix=/50"
+	src="https://progress-bar.dev/35/?scale=50&title=StarsCollected&width=700&suffix=/50"
 	alt="Progress Bar">
 </img>
 | Day | Status | Source | Solution Notes |
@@ -20,6 +20,7 @@
  | [Day 09:  Mirage Maintenance](https://adventofcode.com/2023/day/9) | :star::star: | [Solution](./Day09/Program.cs) |  |
  | [Day 10:  Pipe Maze](https://adventofcode.com/2023/day/10) | :star::star: | [Solution](./Day10/Program.cs) |  |
  | [Day 11:  Cosmic Expansion](https://adventofcode.com/2023/day/11) | :star::star: | [Solution](./Day11/Program.cs) |  |
+ | [Day 12:  Hot Springs](https://adventofcode.com/2023/day/12) | :star: | [Solution](./Day12/Program.cs) |  |
  | [Day 13:  Point of Incidence](https://adventofcode.com/2023/day/13) | :star: | [Solution](./Day13/Program.cs) |  |
  | [Day 14:  Parabolic Reflector Dish](https://adventofcode.com/2023/day/14) | :star::star: | [Solution](./Day14/Program.cs) |  |
  | [Day 15:  Lens Library](https://adventofcode.com/2023/day/15) | :star::star: | [Solution](./Day15/Program.cs) |  |
@@ -27,4 +28,5 @@
  | [Day 17:  Clumsy Crucible](https://adventofcode.com/2023/day/17) | :star: | [Solution](./Day17/Program.cs) |  |
  | [Day 18:  Lavaduct Lagoon](https://adventofcode.com/2023/day/18) | :star::star: | [Solution](./Day18/Program.cs) |  |
  | [Day 19:  Aplenty](https://adventofcode.com/2023/day/19) | :star: | [Solution](./Day19/Program.cs) |  |
+ | [Day 21:  Step Counter](https://adventofcode.com/2023/day/21) | :star: | [Solution](./Day21/Program.cs) |  |
 
