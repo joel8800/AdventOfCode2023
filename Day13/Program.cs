14a3452fdd7f35d7f1dc10152f6d2367dbe81da0
Joel Kaneshiro <joel8800@gmail.com>
2023-12-29 21:15:19 -0800

    Completed Day13 part 1

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -5,55 +5,35 @@ Console.WriteLine("Day13: Point of Incidence");
 
 string[] blocks = FileUtil.ReadFileByBlock("input.txt");
 
-int sum = 0;
-List<int> blockRefs = [];
+int sumPt1 = 0;
 List<(int h, int v)> results = [];
 
 foreach (string block in blocks)
 {
     List<string> grid = [.. block.Split(Environment.NewLine)];
-
-    for (int r = 0; r < grid.Count; r++)
-    {
-        for (int c = 0; c < grid[0].Length; c++)
-            Console.Write(grid[r][c]);
-        Console.WriteLine();
-    }
-
-    int h = CheckHorizontalReflection(grid, (-1, -1));
+    
+    int h = CheckHorizontalReflection(grid, (-1, -1), false);
     int v = CheckVerticalReflection(grid, (-1, -1));
 
-    results.Add((h, v));
-    Console.WriteLine($"[{h,3},{v,3}]");
-    Console.WriteLine();
+    results.Add((h, v));    // save results for part2
 
     if (h != -1)
-    {
-        blockRefs.Add(h * 100);
-        sum += h * 100;
-    }
+        sumPt1 += h * 100;
+
     if (v != -1)
-    {
-        blockRefs.Add(v);
-        sum += v;
-    }
+        sumPt1 += v;
 }
 
-int i = 0;
-foreach ((int h, int v) in results)
-    Console.WriteLine($"{i++}: h={h,3}, v={v,3}");
-
-Console.WriteLine($"Part1: {sum}");
+Console.WriteLine($"Part1: {sumPt1}");
 
 //----------------------------------------------------------------------------
 
-sum = 0;
+int sumPt2 = 0;
 int blockNum = 0;
 foreach (string block in blocks)
 {
     int h = -1;
     int v = -1;
-    bool newScore = false;
 
     List<string> grid = [.. block.Split(Environment.NewLine)];
 
@@ -61,25 +41,16 @@ foreach (string block in blocks)
     {
         for (int cc = 0; cc < grid[0].Length; cc++)
         {
-            Console.Write($"[{blockNum,3}][{cr,2},{cc,2}]  ");
             // flip location
             grid[cr] = FlipLocation(grid[cr], cc);
 
-            //for (int r = 0; r < grid.Count; r++)
-            //{
-            //    for (int c = 0; c < grid[0].Length; c++)
-            //        Console.Write(grid[r][c]);
-            //    Console.WriteLine();
-            //}
-
-            h = CheckHorizontalReflection(grid, results[blockNum]);
+            h = CheckHorizontalReflection(grid, results[blockNum], false);
             v = CheckVerticalReflection(grid, results[blockNum]);
 
             // flip back
             grid[cr] = FlipLocation(grid[cr], cc);
 
-            Console.WriteLine($"og:[{results[blockNum].h,3},{results[blockNum].v,3}]  new:[{h,3},{v,3}]");
-            if ((h, v) != (-1, -1) && (h, v) != results[blockNum])
+           if ((h, v) != (-1, -1) && (h, v) != results[blockNum])
                 break;
         }
         if ((h, v) != (-1, -1) && (h, v) != results[blockNum])
@@ -93,31 +64,33 @@ foreach (string block in blocks)
     }
 
     if (h != -1)
-        sum += h * 100;
+        sumPt2 += h * 100;
     if (v != -1)
-        sum += v;
-    Console.WriteLine($"[{blockNum}] sum = {sum} (h={h,3} v={v,3})");
-    Console.WriteLine();
+        sumPt2 += v;
 
     blockNum++;
 }
 
-Console.WriteLine($"Part2: {sum}");
+Console.WriteLine($"Part2: {sumPt2}");
 
 //============================================================================
 
-int CheckHorizontalReflection(List<string> grid, (int h, int v) pt1Score)
+void PrintGrid(List<string> grid)
+{
+    for (int r = 0; r < grid.Count; r++)
+    {
+        for (int c = 0; c < grid[0].Length; c++)
+            Console.Write(grid[r][c]);
+        Console.WriteLine();
+    }
+}
+
+int CheckHorizontalReflection(List<string> grid, (int h, int v) pt1Score, bool isVert)
 {
     int fold = 0;
-
-    //if (grid[0] == grid[1])
-    //{
-    //    Console.WriteLine("rows 0 & 1 match");
-    //    return 0;
-    //}
-
     string above = "";
     string below = "";
+
     for (int i = 0; i < grid.Count - 1; i++)
     {
         int offset = 0;
@@ -129,33 +102,29 @@ int CheckHorizontalReflection(List<string> grid, (int h, int v) pt1Score)
         {
             above += grid[i - offset];
             below += grid[i + offset + 1];
-            //Console.WriteLine($"a:{i-offset,3}  above:{above}");
-            //Console.WriteLine($"b:{i+offset+1,3}  below:{below}");
-            //Console.WriteLine("----");
             offset++;
         }
 
         fold++;
         if (above == below)
         {
-            if (fold == pt1Score.h || fold == pt1Score.v)
+            if (isVert == false && fold == pt1Score.h)
                 continue;
-            else
-                break;
+            if (isVert == true && fold == pt1Score.v)
+                continue;
+
+            return fold;
         }
     }
 
-    if (above == below)
-        return fold;
-    else
-        return -1;
+    return -1;
 }
 
 int CheckVerticalReflection(List<string> grid, (int h, int v) pt1Score)
 {
     List<string> transposed = Transpose(grid);
 
-    return CheckHorizontalReflection(transposed, pt1Score);
+    return CheckHorizontalReflection(transposed, pt1Score, true);
 }
 
 List<string> Transpose(List<string> grid)
